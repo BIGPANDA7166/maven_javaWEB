8e3941f48a5c4e8007aa255afe00764e6f2761c3
BiigPandaaaaaaa <1414949310@qq.com>
2019-03-11 16:32:39 +0800

    maven_javaSE_day01

---
+++ maven_javaSE_day01/src/main/java/com/heima/maven/App.java
@@ -0,0 +1,13 @@
+package com.heima.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
